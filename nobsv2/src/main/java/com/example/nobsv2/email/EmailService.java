9a978391a943a8a0da3a793eb9d99332fc0d62cc
Ilgert1 <ilgertshehaj6@gmail.com>
2025-12-02 22:28:10 -0500

    skipped mail conn

--- nobsv2/src/main/java/com/example/nobsv2/email/EmailService.java
+++ nobsv2/src/main/java/com/example/nobsv2/email/EmailService.java
@@ -3,6 +3,7 @@ package com.example.nobsv2.email;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class EmailService {
 
+    @Autowired(required = false)
     private final JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
